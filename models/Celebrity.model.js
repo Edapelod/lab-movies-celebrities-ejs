cafc02336a8d0b8bb32be755145b3356c598b9d2
Enrique Dapelo <enricodapelo6@hotmail.com>
2022-10-31 16:50:21 +0100

    iteration2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,20 @@
 //  Add your code here
+
+
+const { Schema, model } = require('mongoose')
+
+const celebritySchema = new Schema({
+  name: {
+    type: String
+  },
+  occupation: {
+    type: [String]
+  },
+  catchPhrase: {
+    type: String
+  },
+})
+
+const celebrityModel = model('celebrity', celebritySchema)
+
+module.exports = celebrityModel
