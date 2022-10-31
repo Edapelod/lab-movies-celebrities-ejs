e1280f1553b966bd6502db6795c532073e4da61d
Enrique Dapelo <enricodapelo6@hotmail.com>
2022-10-31 16:38:36 +0100

    iteration1

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,16 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+
+const index = require('./routes/index')
+app.use('/', index)
+
+
+const movieRoutes = require('./routes/movies.routes')
+app.use('/movies', movieRoutes)
+
+
+const celebritiesRoutes = require('./routes/celebrities.routes')
+app.use('/celebrities', celebritiesRoutes)
+
 module.exports = router;
